42e630249a0497174ad31fbae551dbe887dce8b2
Anson Paul <apaul@zuelligpharma.com>
2023-08-11 21:18:12 -0400

    second test

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -31,4 +31,19 @@ public class AppTest
         Reporter.log("Response=>" + response.prettyPrint());
 }
 
+    @Test
+    public void getTest2()
+    {
+        // Specify the base URL to the RESTful web service 
+        RestAssured.baseURI = "https://demoqa.com/BookStore/v1"; 
+        // Get the RequestSpecification of the request to be sent to the server. 
+        RequestSpecification httpRequest = RestAssured.given(); 
+        // specify the method type (GET) and the parameters if any. 
+        //In this case the request does not take any parameters 
+        Response response = httpRequest.request(Method.GET, "/Books");
+        // Print the status and message body of the response received from the server 
+        //Reporter.log("Status received => " + response.getStatusLine()); 
+        Reporter.log("Response=>" + response.prettyPrint());
+}
+
 }
